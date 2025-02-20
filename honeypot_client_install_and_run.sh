b2e1c6d9aa3efd4e8eea765a974fee1c5c43bafe
astro <sudo.astro@gmail.com>
2025-02-20 19:30:57 +0500

    ...

--- honeypot_client_install_and_run.sh
+++ honeypot_client_install_and_run.sh
@@ -10,17 +10,62 @@ ARCH=$(uname -m)
 echo "🔍 Определена ОС: $OS"
 echo "🔍 Определена архитектура: $ARCH"
 
-# Определение ссылки на ZIP-файл в зависимости от ОС и архитектуры
+# Определение ссылки на бинарник в зависимости от ОС и архитектуры
 if [ "$OS" == "Linux" ]; then
     case "$ARCH" in
         "x86_64")
-            ZIP_URL="https://github.com/AsTR0I/HoneypotAgent/blob/main/honeypot-client/builds/HoneypotAgentLinux386.tar.gz"
+            BIN_URL="https://github.com/AsTR0I/HoneypotAgent/blob/main/honeypot-client/bin/linux/amd64/HoneypotAgent?raw=true"
             ;;
-        "aarch64")
-            ZIP_URL="https://github.com/AsTR0I/HoneypotAgent/blob/main/honeypot-client/builds/HoneypotAgentLinuxamd64.tar.gz"
-        ;;
-    *)
-        echo "❌ Неизвестная архитектура: $ARCH. Скрипт завершает работу."
+        "i386")
+            BIN_URL="https://github.com/AsTR0I/HoneypotAgent/blob/main/honeypot-client/bin/linux/386/HoneypotAgent?raw=true"
+            ;;
+        *)
+            echo "❌ Неизвестная архитектура для Linux: $ARCH. Скрипт завершает работу."
             exit 1
             ;;
     esac
+elif [ "$OS" == "FreeBSD" ]; then
+    case "$ARCH" in
+        "x86_64")
+            BIN_URL="https://github.com/AsTR0I/HoneypotAgent/blob/main/honeypot-client/bin/freebsd/amd64/HoneypotAgent?raw=true"
+            ;;
+        "i386")
+            BIN_URL="https://github.com/AsTR0I/HoneypotAgent/blob/main/honeypot-client/bin/freebsd/386/HoneypotAgent?raw=true"
+            ;;
+        *)
+            echo "❌ Неизвестная архитектура для FreeBSD: $ARCH. Скрипт завершает работу."
+            exit 1
+            ;;
+    esac
+elif [ "$OS" == "Darwin" ]; then
+    echo "❌ macOS не поддерживается. Скрипт завершает работу."
+    exit 1
+elif [ "$OS" == "Windows" ]; then
+    case "$ARCH" in
+        "x86_64")
+            BIN_URL="https://github.com/AsTR0I/HoneypotAgent/blob/main/honeypot-client/bin/windows/amd64/HoneypotAgent?raw=true"
+            ;;
+        *)
+            echo "❌ Неизвестная архитектура для Windows: $ARCH. Скрипт завершает работу."
+            exit 1
+            ;;
+    esac
+else
+    echo "❌ Неизвестная операционная система: $OS. Скрипт завершает работу."
+    exit 1
+fi
+
+# Печать ссылки на бинарник
+echo "🔗 Ссылка для загрузки: $BIN_URL"
+
+# Пример скачивания файла
+echo "📥 Скачиваем файл..."
+curl -L -o HoneypotAgent "$BIN_URL"
+
+# Проверка скачивания
+if [ $? -eq 0 ]; then
+    echo "✅ Бинарник успешно скачан."
+else
+    echo "❌ Ошибка при скачивании бинарника."
+    exit 1
+fi
